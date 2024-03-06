ad93c1c919de16d96c8c448d2c707f45a58e72bd
Sreejaya <itsmesreejaya@gmail.com>
2024-03-06 13:17:08 +0530

    fix: day card has duration now when locked

--- src/components/DaywiseCard.tsx
+++ src/components/DaywiseCard.tsx
@@ -1,73 +1,87 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import CircularProgress from "./CircularProgress";
 import Icon from "react-native-vector-icons/FontAwesome";
 import ilpex from "../utils/ilpexUI";
+import { useNavigation } from "@react-navigation/native";
 
-type PropsType = { Day: number; duration: string; progressValue: number; status: boolean };
+type PropsType = { Day: number; duration: string; progressValue: number; status: boolean; day_id:string };
 
 const Daywise = (props: PropsType) => {
-  const { Day, duration, progressValue, status } = props;
+
+  // const navigation: any = useNavigation();
+
+
+  const { Day, duration, progressValue, status,day_id } = props;
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity>
       <View style={styles.learningDay}>
-        <View>
-          <Text style={styles.learningLabel}>Day {Day}</Text>
-          {!status && (
-            <View style={styles.lockedContainer}>
-              <Icon name="lock" color={ilpex.darkGrey} size={40} style={{ position: 'relative', left: 270, bottom: 15 }} />
-            </View>
-          )}
-          {status && progressValue === 100 ? (
-            <View style={styles.completeContainer}>
-              <Text style={styles.learningTime}>{duration}</Text>
-              <Icon name="check" color={ilpex.success} size={17} style={{ position:'relative', left:135}} />
-              <Text style={styles.completeText}>Complete</Text>
-            </View>
-          ) : status ? (
-            <View>
-              <Text style={styles.learningTime}>{duration}</Text>
-            </View>
-          ) : null}
+        <View style={styles.cardContainer}>
+              <View>
+                <Text style={styles.learningLabel}>Day {Day}</Text>
+                <Text style={styles.learningTime}>{duration}</Text>
+              </View>
+              <View>
+               {status && progressValue === 100 && (
+                            <View style={{flexDirection: 'row'}}>
+                  
+                              <Icon name="check" color={ilpex.success} size={17} />
+                              <Text style={styles.completeText}>Complete</Text>
+                            </View>
+                          )}
+                {!status && (
+                      <View>      
+                        <Icon name="lock" color={ilpex.darkGrey} size={40}/>
+                      </View>
+                    )}
+
+                
+                {status && progressValue !== 100 && (
+                    <View>
+                      <CircularProgress completeStatus={progressValue} />
+                    </View>
+                  )}
+              </View>
+          
+          
         </View>
-        {status && progressValue !== 100 ? (
-          <View>
-            <CircularProgress completeStatus={progressValue} />
-          </View>
-        ) : null}
+        
       </View>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  cardContainer:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    alignItems: 'center',
+  },
 
   container: {
     position:'absolute',
     maxWidth: "100%",
-    padding: 20,
+    padding: 20,  
+
   },
   learningDay: {
     width: 350,
     borderRadius: 5,
-    backgroundColor: ilpex.card,
     padding: 20,
     marginTop: 10,
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
     elevation: 5, // Android shadow
     shadowOffset: { width: 0, height: 5 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    fontWeight: "700",
+    fontWeight: "bold",
     height: 100,
   },
   learningLabel: {
     position:'relative',
-    top:15,
     fontSize: 25,
     color: ilpex.black,
   },
@@ -75,21 +89,12 @@ const styles = StyleSheet.create({
     color: ilpex.darkGrey,
     fontSize: 15,
     position:'relative',
-    top:10,
   },
-  lockedContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
+
   lockedText: {
     fontSize: 14,
     fontWeight: "500",
     color:ilpex.darkGrey,
-  },
-
-  completeContainer: {
-    flexDirection: "row",
-    alignItems: "center",
   },
   completeText: {
     fontSize: 15,
@@ -97,7 +102,6 @@ const styles = StyleSheet.create({
     marginRight: 10,
     color: ilpex.success ,
     position:'relative',
-    left:140,
   },
 });
 
