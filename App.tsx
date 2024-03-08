504fc997de8fd005139e5cf8aeb057e4855d650d
Sreejaya <itsmesreejaya@gmail.com>
2024-03-08 13:07:10 +0530

    feat:forgot password screen completed without api

--- App.tsx
+++ App.tsx
@@ -2,15 +2,13 @@ import {Provider} from 'react-redux';
 import { store } from './src/context/store';
 import Main from './src/Main';
 import React from 'react';
-import ManageUserScreen from './src/screens/admin/ManageUserScreen';
-import { ForgotPasswordScreen } from './src/screens/authentication/ForgotPasswordScreen';
+
 
 const App = () => {
   return (
-    // <Provider store={store}>
-    //   <Main />
-    // </Provider>
-    <ForgotPasswordScreen/>
+    <Provider store={store}>
+      <Main />
+    </Provider>
     );
 };
  
