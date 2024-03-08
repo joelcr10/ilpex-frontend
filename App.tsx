959828ffc430fc9878d6d66bf8cc9bedf8465dc2
Sreejaya <itsmesreejaya@gmail.com>
2024-03-08 13:06:02 +0530

    feat:forgot password screen completed without api

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import { store } from './src/context/store';
 import Main from './src/Main';
 import React from 'react';
 import ManageUserScreen from './src/screens/admin/ManageUserScreen';
+import { ForgotPasswordScreen } from './src/screens/authentication/ForgotPasswordScreen';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Main />
-    </Provider>
+    // <Provider store={store}>
+    //   <Main />
+    // </Provider>
+    <ForgotPasswordScreen/>
     );
 };
  
---
+++ src/screens/authentication/ForgotPasswordScreen.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { Text,View,StyleSheet } from "react-native";
+import BlackHeading from "../../components/BlackHeading";
+import ilpex from "../../utils/ilpexUI";
+import InputField from "../../components/InputField";
+import Button from "../../components/Button";
+
+
+
+
+const ForgotPasswordScreen = ()=>{
+
+    const [email,setEmail] = useState('');
+    const [buttonPressed, setButtonpressed] = useState(false);
+
+    const handleForgotPassword=()=>{
+        setButtonpressed(true)
+    }
+
+   return(
+    <View style={styles.mainView}>
+        <BlackHeading heading={"Forgot Password?"}/>
+        <Text style={styles.text}>
+            To initiate the password reset process, kindly provide your email address below.
+        </Text>
+        <InputField 
+            label={"Email"} 
+            isPassword={false} 
+            value={email} 
+            onChangeText={setEmail}
+        />
+        <Text>{"\n"}{"\n"}</Text>
+        <Button 
+            name={"Submit"} 
+            onPress={handleForgotPassword} 
+            buttonPressed={buttonPressed}/>
+    </View>
+   )
+
+
+}
+
+const styles = StyleSheet.create({
+    mainView:{
+        alignContent:'center',
+        justifyContent:'center',
+        height: '100%',
+    },
+    text:{
+        marginTop:30,
+        color: ilpex.darkGrey,
+        fontFamily:ilpex.fontMedium,
+        fontSize:17,
+        textAlign: 'center',
+        marginLeft:30,
+        maxWidth:350,
+        marginBottom:20, 
+    }
+
+})
+
+export {ForgotPasswordScreen}
