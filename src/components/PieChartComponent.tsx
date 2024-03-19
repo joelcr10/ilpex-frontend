4851736a3347d63c523664d86bb9ba8c9a18339c
Ashik George <ashikgeorgec@gmail.com>
2024-03-19 10:40:38 +0530

    pulling

--- src/components/PieChartComponent.tsx
+++ src/components/PieChartComponent.tsx
@@ -1,15 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-import {StyleSheet, ScrollView, StatusBar, Text, View,Button} from 'react-native';
+import React from 'react';
+import {StyleSheet, ScrollView,Text, View} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import PieChart from 'react-native-pie-chart';
-// import {PieChart} from '../index';
-import * as Animatable from 'react-native-animatable';
-
-import Svg, { Path, G } from 'react-native-svg';
-import Animated, { Easing } from 'react-native-reanimated';
-import { duration } from 'moment';
-
-// import { VictoryPie } from 'victory-native';
 type chart ={excellent:number,
             good:number,
             poor:number,
@@ -24,32 +16,19 @@ type chart ={excellent:number,
             onePointFive:number,
             LessOnePointFive:number
           }
-
-
 const ChartPie = (props:chart) => {
 
   const{chartName,excellent,good,poor,option1,option2,option3,option4,option5,incomplete,option,onePointFive,LessOnePointFive} =props;
   const widthAndHeight = 200
   const series = [excellent,good,poor,onePointFive,LessOnePointFive];
   const sliceColor = [ '#29A662', '#FFA600','#D3583D','blue','black'];
-
-  // const pieData = [
-  //   {value: excellent, color: '#29A662'},
-  //   {value: good, color: '#FFA600'},
-  //   {value: poor, color: '#D3583D'},
-  //   // {value: poor, color: '#D3583D', text:poor},
-  // ];
-
-  
-    return(
+  return(
        <ScrollView style={styles.scrollContainer}>
          <View style={styles.container}>
-  
           <View style={styles.heading}>
           <Text style={{fontFamily : 'Poppins-SemiBold',color:'black',fontSize:20}}>{chartName}</Text>
           </View>
           {excellent==0 && good==0 && poor==0 ?<Text></Text> :
-          
           <View>
           {option == 'assesment' &&
           <View style={styles.head}>
@@ -70,10 +49,8 @@ const ChartPie = (props:chart) => {
             </View>
           </View>}
           </View>}
-            
-          {excellent==0 && good==0 && poor==0 ?<Text style={{fontFamily : 'Poppins-Regular',color:'black',fontSize:14,marginLeft:80}}>Assesments are not started</Text>:
+           {excellent==0 && good==0 && poor==0 ?<Text style={{fontFamily : 'Poppins-Regular',color:'black',fontSize:14,marginLeft:80}}>Assesments are not started</Text>:
            <View style={styles.chart}>
-          {/* <Animatable.View animation="fadeIn" duration={8000} style={{ flex: 1 }}> */}
                <PieChart
                 widthAndHeight={widthAndHeight}
                 series={series}
@@ -82,8 +59,6 @@ const ChartPie = (props:chart) => {
                 coverRadius={0.45}
                 coverFill={'#FFF'}
               />
-
-            
          </View>}
          {excellent==0 && good==0 && poor==0 ?<Text></Text> :
         <View style={styles.bottom}>
@@ -126,8 +101,6 @@ const ChartPie = (props:chart) => {
             <Text style={{fontFamily : 'Poppins-Medium',color:'#6D6D6D',fontSize:15}}>{option2}</Text>
             </TouchableOpacity>
           </View>}
-          
-  
           <View>
           <TouchableOpacity onPress={()=>incomplete(3)}>
             <View style={styles.footer}>
@@ -137,25 +110,16 @@ const ChartPie = (props:chart) => {
             <Text style={{fontFamily : 'Poppins-Medium',color:'#6D6D6D',fontSize:15}}>{option3}</Text>
             </TouchableOpacity>
           </View>
-
-          
-          
-  
           </View>}
         </View>
       </ScrollView>
     );
-
-  
 }
  
-
 const styles = StyleSheet.create({
   scrollContainer: {
      flex: 1,
-    //  height : 440,
-  
-  },
+   },
   container: {
     marginTop:10,
     marginHorizontal:30,
@@ -169,12 +133,7 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   heading:{
-    
-    //borderWidth:2,
-    //marginLeft:25,
-   
     margin:20
-    
   },
   chart:{
       alignItems:'center',
@@ -211,7 +170,6 @@ const styles = StyleSheet.create({
     borderRadius:50
   },
   bottom:{
-    
     display:'flex',
     flexDirection:'row',
     justifyContent:'space-evenly',
@@ -229,7 +187,6 @@ const styles = StyleSheet.create({
     marginTop : 10,
     marginBottom : 10,
     fontSize:14,
-    // color:"black",
     fontFamily : 'Poppins-Medium',
     color : '#6D6D6D',
   },
@@ -237,7 +194,6 @@ const styles = StyleSheet.create({
     display:'flex',
     flexDirection:'row',
     justifyContent:'space-evenly',
-    // marginHorizontal:30,
   },
   headPercentage:{
     display:'flex',
@@ -245,7 +201,6 @@ const styles = StyleSheet.create({
     marginBottom:5,
   },
 });
-
 export default ChartPie;
 
 
