907a56a46d34e38fa6c10f62d6e33fc1d0e4df09
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-03-19 10:47:49 +0530

    Feat : Modified Incomplete Trainees Screen API

--- src/screens/admin/IncompleteTraineesScreen.tsx
+++ src/screens/admin/IncompleteTraineesScreen.tsx
@@ -45,12 +45,12 @@ const IncompleteTraineesScreen = () => {
       const getDayCards = async () => {
         try {
           const { responseData } = await getHook(
-            `/api/v2/batch/${batch}/pending/day/${day}`,
+            `api/v2/batch/${batch}/incompleteTrainees/day/${day}`,
           );
           if (responseData) {
             
             setTraineeList(responseData);
-            console.log(responseData.IncompleteTraineeList);
+            console.log(responseData.data);
 
           }
 
@@ -78,11 +78,11 @@ const IncompleteTraineesScreen = () => {
                <FlatList
           scrollEnabled={false}
           showsVerticalScrollIndicator={false}
-          data={traineeList.IncompleteTraineeList}
+          data={traineeList.data}
           renderItem={({ item }) => (
             <IncompleteTraineeCard
-              trainee_name={item.user_name}
-              batch_name={item.Batch} courses_left={item.incomplete_courses} total_number_of_courses={item.total_courses} course_list={item.incomplete_courses_list}  />
+              trainee_name={item.traineeName}
+              batch_name={item.batchName} courses_left={item.coursesLeft} total_number_of_courses={item.totalNumberOfCourses} course_list={item.incompleteCourseList}  />
           )}
           keyExtractor={item => item.id}
         />
