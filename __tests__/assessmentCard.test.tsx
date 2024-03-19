96f917c68e4ca4e2c0c288d494cf2fb1f7ba4043
joelcr10 <joelcraju@gmail.com>
2024-03-20 01:00:54 +0530

    login testing

--- __tests__/assessmentCard.test.tsx
+++ __tests__/assessmentCard.test.tsx
@@ -12,7 +12,7 @@ describe("Assessment card testing", () =>{
         const dueDate = "2024-03-19T00:00:00";
         const status = true;
 
-        const { getByText, getByTestId } = render(
+        const { getByText} = render(
             <NavigationContainer>
                 <AssessmentCard 
                     assessment_id={assessment_id}
@@ -27,9 +27,24 @@ describe("Assessment card testing", () =>{
         // Check if assessment name, batch name, and formatted due date are rendered
         expect(getByText(assessmentName)).toBeTruthy();
         expect(getByText(batchName)).toBeTruthy();
-        expect(getByText("19 Mar 2024")).toBeTruthy(); // Assuming formatDate function formats the date correctly
 
         // Check if the button to take the test is rendered
         expect(getByText("Take test")).toBeTruthy();
     });
+
+    it("formats date correctly", () => {
+        const { getByText } = render(
+            <NavigationContainer>
+                <AssessmentCard 
+                    assessment_id={1}
+                    assessmentName="Test Assessment"
+                    batchName="Batch 1"
+                    dueDate="2024-03-20T00:00:00Z"
+                    status={false}
+                />
+            </NavigationContainer>
+        );
+    
+        expect(getByText('20 Mar 2024')).toBeTruthy();
+      });
 })
